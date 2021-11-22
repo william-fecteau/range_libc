3196147df014125e0ea5e70311638909675cd487
billyzheng-mlab <billyzheng.bz@gmail.com>
2021-11-22 15:38:53 -0500

    remove linking in compile script

--- pywrapper/compile_with_cuda.sh
+++ pywrapper/compile_with_cuda.sh
@@ -5,7 +5,4 @@ sudo apt install -y gcc-8 g++-8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
 
-# linking cudart library
-sudo ln -s /usr/local/cuda-10.0/lib64/libcudart.so /usr/local/cuda/lib64/libcudart.so
-
 sudo WITH_CUDA=ON python3 setup.py install
